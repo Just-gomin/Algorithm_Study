701c17e48407f146c671234a3dba43ea67d280a1
An KangMin <hkkm612@gmail.com>
2021-01-05 08:06:21 +0900

    2020.01.05_Programmers_후보키(4차시도-해결)

--- programmers/난이도/Level2/후보키/code/candidateKey.js
+++ programmers/난이도/Level2/후보키/code/candidateKey.js
@@ -6,11 +6,8 @@
     2. 후보키는 1개의 속성만이 아니라 2개 이상의 조합으로 구성될 수 있습니다.
 
     # 문제 해결 방안
-    1. 후보키를 구성하는 속성의 수를 하나씩 늘려가며 확인합니다.
-    2. 1개의 속성으로 후보키를 검사하는 방법은 해당 속성의 값을 튜플들로 부터 추출하고, Set으로 만듭니다. 해당 Set의 길이와 튜플의 수가 동일하다면 해당 속성은 후보키가 될 수 있습니다.
-    3. 후보키가 되지 못한 속성들을 추려서 조합합니다.
-    4. 생성한 조합의 속성들로 후보키가 되는지 확인합니다.
-    5. 후보키가 되지 못한 속성들로 3,4를 반복합니다.
+    1. 1개의 속성 자체만으로도 후보키가 되는 경우와, 2가지 이상의 속성의 조합이 후보키가되는 경우를 나누어 생각합니다.
+    2. 2개 이상의 속성의 조합이 후보키가 되는 경우, (2 ~ 해당 조합의 속성 수 -1)개의 속성 조합으로 된 후보키들과 비교해 최소성을 만족하는지 확인합니다.
 */
 
 // nCr
@@ -35,10 +32,21 @@ const solution = (relation = []) => {
   let ntuple = relation.length,
     nattribute = relation[0].length;
   let remainIDX = [];
-  for (let i = 0; i < nattribute; i++) remainIDX.push(i);
 
-  for (let i = 1; i <= nattribute; i++) {
-    let combinations = get_nCr(i, remainIDX);
+  // 1개 속성이 후보키가 되는 경우
+  for (let i = 0; i < nattribute; i++) {
+    let tempSet = relation.map((tuple) => tuple[i]);
+    let checkSet = new Set(tempSet);
+    if (checkSet.size === ntuple) result.push([i]);
+    else remainIDX.push(i);
+  }
+
+  // 2개 이상의 속성이 후보키가 되는 경우
+  for (let itr = 2; itr <= remainIDX.length; itr++) {
+    let minimalityChecker = result.filter((candidate) => candidate.length >= 2);
+    let tempCandidates = [];
+    let combinations = get_nCr(itr, remainIDX);
+
     combinations.forEach((combination) => {
       let tempSet = [];
       relation.forEach((tuple) => {
@@ -46,14 +54,24 @@ const solution = (relation = []) => {
         combination.forEach((attr) => (checker += tuple[attr]));
         tempSet.push(checker);
       });
+
+      // 해당 조합이 후보키가 되는지 확인.
       let checkSet = new Set(tempSet);
       if (checkSet.size === ntuple) {
-        combination.forEach(
-          (attr) => (remainIDX = remainIDX.filter((value) => value != attr))
-        );
-        result.push(combination);
+        // 후보키가 최소성을 만족하는지 확인
+        let minimality = true;
+        minimalityChecker.forEach((candidate) => {
+          let counter = 0;
+          candidate.forEach((attr) => {
+            if (combination.findIndex((value) => value === attr) !== -1)
+              counter++;
+          });
+          if (counter == candidate.length) minimality = false;
+        });
+        if (minimality) tempCandidates.push(combination);
       }
     });
+    result.push(...tempCandidates);
   }
   return result.length;
 };
