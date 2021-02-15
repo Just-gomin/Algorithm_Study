72c4d8440a605fdb0ad8ca7946547d19fae87e90
Kangmin An <hkkm612@gmail.com>
2021-02-15 21:56:51 +0900

    2021.02.15_Programmers_가장 먼 노드(2차시도-해결,JS)

--- programmers/난이도/Level3/가장 먼 노드/code/solution.js
+++ programmers/난이도/Level3/가장 먼 노드/code/solution.js
@@ -4,45 +4,72 @@
     1. 1번 노드로 부터 가장 멀리 떨어진 노드의 수를 구합니다.
     
     # 문제 해결 방안
-    1. 주어진 간선의 정보를 이용해 그래프를 행렬로 표현합니다.
-    2. 플로이드 알고리즘을 이용해 각 node로 부터 1번(index=0)까지의 최소 거리를 계산합니다.
-    3. 거리가 가장 먼 노드들의 수를 셉니다.
+    1. 주어진 간선의 정보를 이용해 그래프를 리스트로 표현합니다.
+    2. BFS를 통해 1번 node로 부터 떨어진 거리를 구합니다.
+    3. 이미 거리가 구해져 있는 노드의 경우 큐에 넣지않습니다.
+    4. 모든 노드에서 1번 노드까지의 거리가 구해지면, 가장 멀리 있는 노드들의 수를 셉니다.
 
-    * 플로이드 알고리즘의 경우 node의 수가 늘어남에 따라 연산량이 많아져, 실행이 중단되었습니다. 다익스트라 알고리즘으로 접근하도록 합니다.
+    * https://sarah950716.tistory.com/12 를 참고.
+     처음엔 인접 행렬 방식으로 접근하여, 모든 경우를 다 확인하느라 시간이 많이 늘어났습니다.
+     인접 리스트 방식을 채택하여, 확실히 연결된 node들 중에서만 판별을 하니 시간이 대폭 감소하였습니다.
 */
 
 const solution = (n = 2, vertexes = []) => {
-  let distances = Array(n)
+  let graph = Array(n + 1)
     .fill(0)
-    .map((_) => Array(n).fill(9999));
+    .map((_) => Array());
 
-  for (let i = 0; i < n; i++) distances[i][i] = 0;
+  let distances = Array(n + 1).fill(0);
+  let queue = [];
+
+  const infoObj = (parentnode, node) => {
+    return { pNode: parentnode, cNode: node };
+  };
+
   vertexes.forEach((vertex) => {
-    distances[vertex[0] - 1][vertex[1] - 1] = 1;
-    distances[vertex[1] - 1][vertex[0] - 1] = 1;
+    graph[vertex[0]].push(vertex[1]);
+    graph[vertex[1]].push(vertex[0]);
   });
 
-  for (let stop = 0; stop < n; stop++) {
-    for (let start = 0; start < n; start++) {
-      for (let destination = 0; destination < n; destination++) {
-        if (
-          distances[start][destination] >
-          distances[start][stop] + distances[stop][destination]
-        )
-          distances[start][destination] =
-            distances[start][stop] + distances[stop][destination];
-      }
+  graph[1].forEach((node) => {
+    let obj = infoObj(1, node);
+    queue.splice(0, 0, obj);
+  });
+
+  while (distances[0] !== n - 1) {
+    const { pNode, cNode } = queue.pop();
+    if (distances[cNode] === 0) {
+      distances[cNode] = distances[pNode] + 1;
+      distances[0] += 1;
+      graph[cNode].forEach((node) => {
+        if (node !== 1 && distances[node] === 0) {
+          let obj = infoObj(cNode, node);
+          queue.splice(0, 0, obj);
+        }
+      });
     }
   }
-  let max = -9999;
+
+  let max = 0;
   let count = 0;
-  for (let i = 1; i < n; i++) {
-    if (distances[i][0] != 9999 && distances[i][0] > max) {
-      max = distances[i][0];
+  for (let i = 2; i <= n; i++) {
+    if (distances[i] > max) {
+      max = distances[i];
       count = 1;
-    } else if (distances[i][0] == max) {
-      count += 1;
-    }
+    } else if (distances[i] === max) count += 1;
   }
+
   return count;
 };
+
+console.log(
+  solution(6, [
+    [3, 6],
+    [4, 3],
+    [3, 2],
+    [1, 3],
+    [1, 2],
+    [2, 4],
+    [5, 2],
+  ])
+); // result : 3
