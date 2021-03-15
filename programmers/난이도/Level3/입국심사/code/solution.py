40a33f84340b47d89b2d0cea0f1ac07e4acd16df
Kangmin An <hkkm612@gmail.com>
2021-03-15 15:42:21 +0900

    2021.03.15_Programmers_입국심사(2차시도-해결,Py)

--- programmers/난이도/Level3/입국심사/code/solution.py
+++ programmers/난이도/Level3/입국심사/code/solution.py
@@ -11,14 +11,13 @@
     # 문제 해결 방법
     1. 심사하는 데 걸리는 시간을 키로, 해당 시간이 걸리는 심사관의 수를 값으로 하는 dictionary를 생성합니다.
     2. 이분 탐색을 진행합니다. left = 0, right = min(times)*n
-    3. 대기자 수와 같거나 left = right이 되는 순간 까지 반복하여 탐색합니다.
+    3. left <= right 인 상태일 동안만 반복을 진행합니다.
 
     # 참고 https://velog.io/@ollabu3/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9E%85%EA%B5%AD%EC%8B%AC%EC%82%AC
 """
 
 
 def solution(n=0, times=[]):
-    answer = 0
     officers = {}
     mini = times[0]
     for time in times:
@@ -32,7 +31,9 @@ def solution(n=0, times=[]):
     left = 0
     right = mini * n
 
-    while left < right:
+    answer = mini * n
+
+    while left <= right:
 
         mid = (left+right)//2
         inspectors = 0
@@ -40,14 +41,13 @@ def solution(n=0, times=[]):
             inspectors += (mid//time) * officers[time]
 
         if inspectors < n:
-            left = mid
-        elif inspectors > n:
-            right = mid
-        elif inspectors == n or left == right:
-            answer = mid
-            break
+            left = mid + 1
+        else:
+            if mid < answer:
+                answer = mid
+            right = mid - 1
 
     return answer
 
 
-print(solution(7, [5, 10]))
+print(solution(6, [7, 10]))
