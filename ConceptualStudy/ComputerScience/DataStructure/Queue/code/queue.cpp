770ccec227bc395a2d46a2489e58bea6f1581f22
Kangmin An <hkkm612@gmail.com>
2021-04-15 12:02:51 +0900

    Update-ConceptualStudy_Queue(C++)

--- ConceptualStudy/ComputerScience/DataStructure/Queue/code/queue.cpp
+++ ConceptualStudy/ComputerScience/DataStructure/Queue/code/queue.cpp
@@ -20,62 +20,67 @@ template<typename T>
 
 class Queue{
 private:
-    T* q;
-    int header;
-    int tail;
-    int max_size;
+    T* q;       // 배열 기반 큐
+    int header; // 첫 번째 데이터를 가리키는 인데스 (헤더)
+    int tail;   // 마지막 데이터를 가리키는 인덱스 (테일)
+    int arr_size;   // 배열의 최대 크기
 public:
-    Queue(int qsize = 10){
-        q = (T *)(malloc(sizeof(T)*qsize));
-        header = 0;
-        tail = 0;
-        max_size = qsize;
+    Queue(int qsize = 10){                  // Queue Class의 생성자 
+        q = (T *)(malloc(sizeof(T)*qsize)); // 배열 기반 큐의 동적 할당
+        header = 0;                         // header = 0으로 초기화
+        tail = 0;                           // tail = 0으로 초기화
+        arr_size = qsize;                   // 배열의 크기 초기화
     }
     
-    ~Queue(){
-        free(q);
+    ~Queue(){       // Queue Class의 소멸자
+        free(q);    // 동적할당된 메모리 해제
         cout << "Queue의 메모리가 해제되었습니다." << endl;
     }
 
-    bool isEmpty(){
-        return header == tail;
-    };
-    bool isFull(){
-        return ((tail + 1) % max_size == header);
+    bool isEmpty(){                 // Queue가 비어있는지 확인하는 멤버 함수
+        return header == tail;      // tail과 header가 같은 곳을 가리키는지 여부 반환
     };
 
-    void enqueue(const T& item){
-        if(!isFull()){
+    bool isFull(){                  // Queue가 가득차있는지 확인하는 멤버 함수
+        return ((tail + 1) % arr_size == header);   // tail의 다음 인덱스가 header인지 확인
+    };
+
+    void enqueue(const T& item){    // Queue에 item을 삽입하는 멤버함수
+        if(!isFull()){              // Queue가 가득차있지 않은 경우 삽입
             q[tail] = item;
             cout << "Enqueue - " << tail << " : " << item << endl;
-            tail = (tail + 1) % max_size;
+            tail = (tail + 1) % arr_size;
         } else {
             cout << "Queue is Full. Remove Item First." << endl;
         }
     }
 
-    void dequeue(){
-        if(!isEmpty()){
+    T dequeue(){            // Queue에서 item을 추출하는 멤버함수
+        if(!isEmpty()){     // Queue가 비어있지 않은 경우 추출
             T rmItem = q[header];
             cout << "Dequeue - " << header << " : " << rmItem << endl;
-            header = (header + 1) % max_size;
+            header = (header + 1) % arr_size;
+            return rmItem;
         } else {
             cout << "Queue is Empty. Add Item First." << endl;
+            return 0;
         }
     };
 
-    T frontData(){
-        return q[tail-1];
+    T frontData(){          // 가장 앞에 있는 데이터를 반환하는 멤버함수
+        return q[header];
     };
 
-    T rearData(){
-        return q[header];
+    T rearData(){           // 가장 뒤에 있는 데이터를 반환하는 멤버함수
+        return q[tail-1];
     }
 
-    void printQueue(){
+    void printQueue(){      // Queue에 들어있는 모든 원소들을 출력하는 함수
         cout << "------ Front ------" << endl;
-        for(int i = tail-1; i >= header ; i--){
-            cout <<"Node " << i << " : " << q[i] << endl;
+        int idx = header;
+        while(idx != tail){
+            cout <<"Node "  << idx << " : " << q[idx] << endl;
+            idx = (idx + 1) % arr_size;
         }
         cout << "------  Rear  ------" << endl;
     }
@@ -166,6 +171,10 @@ int main(void){
         case 'q':
             cout << "프로그램을 종료합니다." << endl;
             loopCondition = false;
+            break;
+        
+        default:
+            cout << "명령어를 다시 입력해주세요." << endl;
             break;
         }
     }
