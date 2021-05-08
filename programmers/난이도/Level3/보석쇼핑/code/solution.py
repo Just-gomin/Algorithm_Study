dfab127e78735a1631ffe55ea02c224e0eab492e
Kangmin An <hkkm612@gmail.com>
2021-05-08 13:54:52 +0900

    2021.05.08_Programmers_보석쇼핑(3차시도-해결,Py)

--- programmers/난이도/Level3/보석쇼핑/code/solution.py
+++ programmers/난이도/Level3/보석쇼핑/code/solution.py
@@ -6,39 +6,37 @@
     3. Set 자료형을 이용해 중복이 없이 보석의 종류를 알아낼 수 있습니다.
 
     # 문제 해결 방법
-    1. Set을 통해 보석의 종류들을 알아냅니다.
-    2. 진열대를 순회하며, {"보석":보석 위치 , ...} 와 같은 형의 바구니에 담으며 기록합니다.
-    3. 바구니의 길이가 보석의 종류와 같으면 해당 구간과 길이를 저장합니다.
-    4. 진열대의 마지막까지 진행하며 가장 짧은 구간을 탐색합니다.
+    1. 문제 해설 방법과 동일
 
-    # 효율성 마지막 테스트 케이스를 통과하지 못했습니다.
+    # 카카오 테크 블로그의 문제 해설을 읽어본 뒤 해결하였습니다.
+        - Link : https://tech.kakao.com/2020/07/01/2020-internship-test/
 """
 
 
 def solution(gems=[]):
     gem_list = set(gems)
+    min_info = {'length' : len(gems) + 1, 'section':[1,len(gems)]}
+    start = 0
+    end = 0
+    basket = {gems[start] : 1}
 
-    if len(gem_list) == len(gems):
-        return [1, len(gems)]
-
-    basket = {}
-    min_info = {'length':len(gems)+1, 'section':[1,1]}
-
-    for i, gem in enumerate(gems):
-        if gem in basket:
-            del basket[gem]
-
-        basket[gem] = i
-
+    while start < len(gems) and end < len(gems):
         if len(basket) == len(gem_list):
-            pos = list(basket.values())
-            start = pos[0]
-            fin = pos[-1]
+            if end - start + 1 < min_info['length']:
+                min_info['length'] = end - start + 1
+                min_info['section'] = [start + 1, end + 1]
+            basket[gems[start]] -= 1
+            if basket[gems[start]] == 0:
+                del basket[gems[start]]
+            start += 1
+        
+        else:
+            end += 1
+            if end >= len(gems):
+                break
+            if gems[end] in basket :
+                basket[gems[end]] += 1
+            else:
+                basket[gems[end]] = 1
 
-            if min_info['length'] > fin - start - 1:
-                min_info['length'] = fin - start - 1
-                min_info['section'] = [start + 1, fin + 1]
-    
     return min_info['section']
-
-
