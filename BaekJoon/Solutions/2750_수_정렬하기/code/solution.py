8da5940b0a11b106a850b078d0471c2b3b3d508b
Kangmin An <hkkm612@gmail.com>
2021-09-18 10:40:52 +0900

    2021.09.18_BOJ_2750_수_정렬하기(알고리즘 변경)

--- BaekJoon/Solutions/2750_수_정렬하기/code/solution.py
+++ BaekJoon/Solutions/2750_수_정렬하기/code/solution.py
@@ -3,6 +3,10 @@ import sys
 n = int(sys.stdin.readline())
 numbers = [int(sys.stdin.readline()) for _ in range(n)]
 
-numbers.sort()
+# Bubble Sort
+for i in range(n-1):
+    for j in range(i+1, n):
+        if numbers[i] > numbers[j]:                          # 큰 숫자가 앞에 있는지 확인
+            numbers[i], numbers[j] = numbers[j], numbers[i]  # 숫자 교환
 
 sys.stdout.write("\n".join(map(str, numbers)))
