12c40630abe4968995c2a55dceb5cea553bb85a6
Kangmin An <hkkm612@gmail.com>
2021-10-05 19:33:39 +0900

    2021.10.05_BOJ_2580_스도쿠(2차시도-해결)

--- BaekJoon/Solutions/2580_스도쿠/code/solution.py
+++ BaekJoon/Solutions/2580_스도쿠/code/solution.py
@@ -1,75 +1,46 @@
-from collections import deque
 from sys import stdin, stdout
 
-board = deque()
-candidates = deque()
-
-# 스도쿠 판의 입력과 빈칸에 대한 후보지 생성
-for i in range(9):
-    line = deque(map(int, stdin.readline().split()))
-    zero_basket = deque()
-    numbers = deque([n for n in range(1, 10)])
-    for j, val in enumerate(line):
-        if val != 0:
-            numbers.remove(val)
-        else:
-            zero_basket.append([i, j, []])
-
-    if len(zero_basket) > 0:
-        for item in zero_basket:
-            candidate = item[:]
-            candidate[2] = numbers.copy()
-            candidates.append(candidate)
-    board.append(line)
+board = [list(map(int, stdin.readline().split())) for _ in range(9)]
+blanks = [(row, col) for row in range(9)
+          for col in range(9) if board[row][col] == 0]
 
 
-def isRight(row, col, target):
-    # 해당 위치에 올바를 숫자가 들어 갔는지 확인하는 함수
-    # 해당 행에 같은 숫자가 있는지 확인
-    if target in board[row]:
-        return False
+def makeCandidates(row, col):
+    # 빈칸에 들어갈 수 있는 숫자들을 반환하는 함수
+    numbers = [n for n in range(1, 10)]
 
-    # 해당 열에 같은 숫자가 있는지 확인
-    for drow in range(9):
-        if board[drow][col] == target:
-            return False
+    # 행과 열에 존재하는 숫자 제거
+    for d in range(9):
+        if board[row][d] in numbers:
+            numbers.remove(board[row][d])
+        if board[d][col] in numbers:
+            numbers.remove(board[d][col])
 
-    # 3*3 내에 같은 숫자가 있는지 확인
-    box_centers = [(1, 1), (1, 4), (1, 7), (4, 1), (4, 4),
-                   (4, 7), (7, 1), (7, 4), (7, 7)]
-    box_shape = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
-                 (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+    # 해당 좌표의 숫자가 속한 3*3 사각형에 포함된 숫자 제거
+    center_row = row // 3
+    center_col = col // 3
+    for dr in range(center_row*3, (center_row+1)*3):
+        for dc in range(center_col*3, (center_col+1)*3):
+            if board[dr][dc] in numbers:
+                numbers.remove(board[dr][dc])
 
-    min_val = 99
-    center = (0, 0)
-    for (drow, dcol) in box_centers:
-        dist = abs(row-drow) + abs(col-dcol)
-        if dist < min_val:
-            min_val = dist
-            center = (drow, dcol)
-
-    for (drow, dcol) in box_shape:
-        if target == board[center[0] + drow][center[1] + dcol]:
-            return False
-
-    return True
+    return numbers
 
 
 def solveSudoku(count):
     # 후보자들을 바탕으로 빈칸을 채워가는 함수
-    if count == len(candidates):
-        return
-    i, j = candidates[count][0], candidates[count][1]
-    candidate = candidates[count][2].copy()
+    if count == len(blanks):
+        for line in board:
+            stdout.write(" ".join(map(str, line)) + "\n")
+        exit(0)
 
-    for n in candidate:
-        if isRight(i, j, n):
-            board[i][j] = n
-            solveSudoku(count+1)
+    (row, col) = blanks[count]
+    candidates = makeCandidates(row, col)
+
+    for n in candidates:
+        board[row][col] = n
+        solveSudoku(count+1)
+        board[row][col] = 0
 
 
 solveSudoku(0)
-
-
-for line in board:
-    stdout.write(" ".join(map(str, line)) + "\n")
