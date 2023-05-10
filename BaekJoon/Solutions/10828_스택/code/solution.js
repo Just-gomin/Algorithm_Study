5050acd9aa3c56c350a6088df30f40b8c9710b33
안강민 <hkkm612@gmail.com>
2023-05-11 01:43:31 +0900

    2023.05.11_BOJ_10828_스택(해결, javascript)

--- BaekJoon/Solutions/10828_스택/code/solution.js
+++ BaekJoon/Solutions/10828_스택/code/solution.js
@@ -1,19 +1,9 @@
-// Read Inputs (from. https://help.acmicpc.net/language/info [node.js & TypeScript])
 const fs = require("fs");
-const { exit } = require("process");
-
-// 입력값 파일 경로 결정
-// node 실행 시 환경변수 [RUNNING_ON]을 "my"라는 값을 부여하여 '백준 온라인' 과 '로컬' 환경 구분
 const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
-
-// 한줄 입력
-// const input = fs.readFileSync(filePath).toString();
-
-// 첫 번째 줄에는 입력 값의 길이, 두 번째 줄부터 실제 데이터
 const [n, ...input] = fs.readFileSync(filePath).toString().split("\n");
 
 var stack = [];
-var answer = []; // console.log 는 시간을 많이 소모 하는 명령이기 때문에 정답을 모아놓았다가, 한번에 출력하기 위함
+var answer = [];
 
 function stackMethod(command, value = 0) {
   switch (command) {
