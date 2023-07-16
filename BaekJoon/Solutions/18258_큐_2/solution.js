82ab3b9841e7225ed6464b3d259c29c704ffc780
안강민 <hkkm612@gmail.com>
2023-07-16 21:16:40 +0900

    BOJ_18258_큐_2(2차시도, 실패)

--- BaekJoon/Solutions/18258_큐_2/solution.js
+++ BaekJoon/Solutions/18258_큐_2/solution.js
@@ -8,57 +8,34 @@ const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
 
 const input = fs.readFileSync(filePath).toString().split("\n");
 
-const queue = {
-  data: [],
-  push: function (val) {
-    this.data.push(val);
-    return;
-  },
-  pop: function () {
-    if (this.size() === 0) {
-      return "-1";
-    }
-    return this.data.shift();
-  },
-  size: function () {
-    return this.data.length;
-  },
-  empty: function () {
-    if (this.size() === 0) {
-      return "1";
-    }
-    return "0";
-  },
-  front: function () {
-    if (this.size() === 0) {
-      return "-1";
-    }
-    return this.data[0];
-  },
-  back: function () {
-    if (this.size() === 0) {
-      return "-1";
-    }
-    return this.data[this.size() - 1];
-  },
-};
+let queue = [];
 
 function solution(data) {
   const [cmd, val] = data.split(" ");
 
   switch (cmd) {
     case "push":
-      return queue.push(val);
+      queue.push(val);
+      return;
     case "pop":
-      return queue.pop();
+      if (queue.length === 0) {
+        return -1;
+      }
+      return queue.shift();
     case "size":
-      return queue.size();
+      return queue.length;
     case "empty":
-      return queue.empty();
+      return queue.length === 0 ? 1 : 0;
     case "front":
-      return queue.front();
+      if (queue.length === 0) {
+        return -1;
+      }
+      return queue[0];
     case "back":
-      return queue.back();
+      if (queue.length === 0) {
+        return -1;
+      }
+      return queue[queue.length - 1];
   }
 }
 
@@ -69,8 +46,8 @@ function main(input) {
 
   for (let i = 0; i < n; i++) {
     let result = solution(data[i]);
-    if (result != undefined) {
-      answer.push(result);
+    if (result !== undefined) {
+      answer.push(result + "");
     }
   }
   console.log(answer.join("\n"));
