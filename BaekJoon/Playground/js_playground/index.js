e3cb1fd2880b191bca13b3157a64499f046717ee
안강민 <hkkm612@gmail.com>
2023-07-17 02:18:24 +0900

    [BOJ / 18258_큐_2] 3차 시도, 실패

--- BaekJoon/Playground/js_playground/index.js
+++ BaekJoon/Playground/js_playground/index.js
@@ -9,10 +9,91 @@ const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
 // 입력 값
 const input = fs.readFileSync(filePath).toString().split("\n");
 
+class QNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.size = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(value) {
+    let newNode = new QNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.head.next = this.tail;
+    } else {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
+    this.size += 1;
+  }
+
+  pop() {
+    if (this.size === 0) {
+      return -1;
+    }
+
+    let value = this.head.value;
+    this.head = this.head.next;
+
+    this.size -= 1;
+
+    return value;
+  }
+
+  front() {
+    if (this.size < 1) {
+      return -1;
+    }
+
+    return this.head.value;
+  }
+
+  back() {
+    if (this.size < 1) {
+      return -1;
+    }
+
+    return this.tail.value;
+  }
+
+  empty() {
+    return this.size === 0 ? 1 : 0;
+  }
+}
+
+let queue = new Queue();
+
 function solution(data) {
   /*
     Code Body
   */
+  const [cmd, val] = data.split(" ");
+
+  switch (cmd) {
+    case "push":
+      queue.push(val);
+      return;
+    case "pop":
+      return queue.pop();
+    case "size":
+      return queue.size;
+    case "empty":
+      return queue.empty();
+    case "front":
+      return queue.front();
+    case "back":
+      return queue.back();
+  }
 }
 
 function main(input) {
@@ -21,7 +102,10 @@ function main(input) {
   let answer = []; // console.log 는 시간을 많이 소모 하는 명령이기 때문에 정답을 모아놓았다가, 한번에 출력하기 위함
 
   for (let i = 0; i < n; i++) {
-    answer.push(solution(data[i]));
+    let result = solution(data[i]);
+    if (result !== undefined) {
+      answer.push(result + "");
+    }
   }
   console.log(answer.join("\n"));
 }
--- BaekJoon/Solutions/18258_큐_2/solution.js
+++ BaekJoon/Solutions/18258_큐_2/solution.js
@@ -5,37 +5,89 @@
 const fs = require("fs");
 const { exit } = require("process");
 const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
-
 const input = fs.readFileSync(filePath).toString().split("\n");
 
-let queue = [];
+class QNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.size = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(value) {
+    let newNode = new QNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.head.next = this.tail;
+    } else {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
+    this.size += 1;
+  }
+
+  pop() {
+    if (this.size === 0) {
+      return -1;
+    }
+
+    let value = this.head.value;
+    this.head = this.head.next;
+
+    this.size -= 1;
+
+    return value;
+  }
+
+  front() {
+    if (this.size < 1) {
+      return -1;
+    }
+
+    return this.head.value;
+  }
+
+  back() {
+    if (this.size < 1) {
+      return -1;
+    }
+
+    return this.tail.value;
+  }
+
+  empty() {
+    return this.size === 0 ? 1 : 0;
+  }
+}
+
+let queue = new Queue();
 
 function solution(data) {
   const [cmd, val] = data.split(" ");
 
   switch (cmd) {
     case "push":
-      queue.push(val);
+      queue.push(parseInt(val));
       return;
     case "pop":
-      if (queue.length === 0) {
-        return -1;
-      }
-      return queue.shift();
+      return queue.pop();
     case "size":
-      return queue.length;
+      return queue.size;
     case "empty":
-      return queue.length === 0 ? 1 : 0;
+      return queue.empty();
     case "front":
-      if (queue.length === 0) {
-        return -1;
-      }
-      return queue[0];
+      return queue.front();
     case "back":
-      if (queue.length === 0) {
-        return -1;
-      }
-      return queue[queue.length - 1];
+      return queue.back();
   }
 }
 
