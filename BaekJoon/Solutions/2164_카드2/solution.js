661b36c494aca374043cb5661d56e4c1f24feeeb
안강민 <hkkm612@gmail.com>
2023-07-24 02:16:40 +0900

    [BOJ / 2164_카드2] 해결 및 스터디 문서 갱신

--- BaekJoon/Solutions/2164_카드2/solution.js
+++ BaekJoon/Solutions/2164_카드2/solution.js
@@ -1,12 +1,11 @@
-// Read Inputs (from. https://help.acmicpc.net/language/info [node.js & TypeScript])
+/*
+  - 문제 Link : https://www.acmicpc.net/problem/2164
+*/
+
 const fs = require("fs");
 const { exit } = require("process");
 
-// 입력값 파일 경로 결정
-// node 실행 시 환경변수 [RUNNING_ON]을 "local"라는 값을 부여하여 '백준 온라인' 과 '로컬' 환경 구분
 const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
-
-// 입력 값
 const input = fs.readFileSync(filePath).toString().split("\n");
 
 class QNode {
