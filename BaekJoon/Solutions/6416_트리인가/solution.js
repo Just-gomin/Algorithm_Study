7b5e9c971f94871c2d5ac971fd3c5092afa99d97
안강민 <hkkm612@gmail.com>
2023-08-03 10:48:17 +0900

    [BOJ / 6416_트리인가] 해결 및 스터디 문서 수정

--- BaekJoon/Solutions/6416_트리인가/solution.js
+++ BaekJoon/Solutions/6416_트리인가/solution.js
@@ -1,33 +1,16 @@
-// ------------------------------
-// Input with File System
-// ------------------------------
+/*
+  - 문제 Link : https://www.acmicpc.net/problem/6416
+*/
 
-// Read Inputs (from. https://help.acmicpc.net/language/info [node.js & TypeScript])
 const fs = require("fs");
 const { exit } = require("process");
-
-// 입력값 파일 경로 결정
-// node 실행 시 환경변수 [RUNNING_ON]을 "local"라는 값을 부여하여 '백준 온라인' 과 '로컬' 환경 구분
 const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
-
-// 입력 값
 const input = fs
   .readFileSync(filePath)
   .toString()
   .trim()
   .split("\n")
   .map((v) => v.trim());
-
-// ------------------------------
-// Input with Read Line
-// ------------------------------
-
-// // Read Inputs (from. https://velog.io/@leenzy/readline-%EB%AA%A8%EB%93%88-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0)
-// const readline = require("readline");
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
 
 const solution = (input) => {
   let answer = [];
@@ -65,6 +48,7 @@ const solution = (input) => {
         let rootNum = 0; // 루트의 개수
         let isTree = true; // 트리인지 여부
 
+        // 노드가 존재하지 않는 트리도 트리이며, 노드가 존재할 때는 아래의 로직에서 판단
         if (nodeSet.size !== 0) {
           for (let node of nodeSet) {
             rootNum += parentMap.has(node) ? 0 : 1;
