ac094c47f210aef8e0a5145e6b7c4678570e25db
안강민 <hkkm612@gmail.com>
2023-08-04 15:59:44 +0900

    create. [BOJ / 11725_트리의_부모_찾기] 1차시도, 시간초과 실패

--- BaekJoon/Solutions/11725_트리의_부모_찾기/solution.js
+++ BaekJoon/Solutions/11725_트리의_부모_찾기/solution.js
@@ -1,33 +1,16 @@
-// ------------------------------
-// Input with File System
-// ------------------------------
+/*
+  - 문제 Link : https://www.acmicpc.net/problem/11725
+*/
 
-// Read Inputs (from. https://help.acmicpc.net/language/info [node.js & TypeScript])
 const fs = require("fs");
 const { exit } = require("process");
-
-// 입력값 파일 경로 결정
-// node 실행 시 환경변수 [RUNNING_ON]을 "local"라는 값을 부여하여 '백준 온라인' 과 '로컬' 환경 구분
 const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
-
-// 입력 값
 const input = fs
   .readFileSync(filePath)
   .toString()
   .trim()
   .split("\n")
   .map((v) => v.trim());
-
-// ------------------------------
-// Input with Read Line
-// ------------------------------
-
-// // Read Inputs (from. https://velog.io/@leenzy/readline-%EB%AA%A8%EB%93%88-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0)
-// const readline = require("readline");
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
 
 function solution(input = []) {
 
