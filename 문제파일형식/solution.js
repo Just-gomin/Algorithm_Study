3e9ce558ff05ae8dcb58d7decf1cb785b2cdea0c
안강민 <hkkm612@gmail.com>
2023-08-04 14:46:46 +0900

    update. [문제파일형식 / JavaScript] 파일 입력 코드 추가(백준 기준으로 풀고 있어서 변경)

--- 문제파일형식/solution.js
+++ 문제파일형식/solution.js
@@ -2,7 +2,32 @@
   - 문제 Link : 
 */
 
+// ------------------------------
+// Input with File System
+// ------------------------------
+const fs = require("fs");
+const { exit } = require("process");
+const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
+
+// ------------------------------
+// Input with Read Line
+// ------------------------------
+
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
 function solution() {
   let answer = "";
   return answer;
 }
+
+console.log(solution(input));
