26320b83cae85f5e7d023e17c6249fdc6f35fdb2
안강민 <hkkm612@gmail.com>
2023-09-20 13:34:20 +0900

    update. [BOJ /21611_마법사_상어와_블리자드] JS 풀이 중.
    - 폭발, 재배치, 재폭발, 변화 각각 함수화 시켜야함

--- BaekJoon/Solutions/21611_마법사_상어와_블리자드/solution.js
+++ BaekJoon/Solutions/21611_마법사_상어와_블리자드/solution.js
@@ -10,6 +10,9 @@
   - 거리 si, 1 ≤ si ≤ (N-1)/2
   - 칸에 들어있는 구슬이 K개라면, 구슬이 들어있는 칸의 번호는 1번부터 K번까지이다.
   - 입력으로 주어진 격자에는 4개 이상 연속하는 구슬이 없다.
+
+  - N, M, 구슬배치(fields) 정보들을 변수에 저장한다.
+  - 
 */
 
 const fs = require("fs");
@@ -22,41 +25,148 @@ const input = fs
   .split("\n")
   .map((v) => v.trim());
 
-class TileInfo {
-  constructor(r, c, bead) {
+class Coordinate {
+  constructor(r, c) {
     this.r = r;
     this.c = c;
+  }
+
+  /**
+   * 위치 변경 함수
+   * @param {number} r 행의 좌표
+   * @param {number} c 열의 좌표
+   */
+  change(r, c) {
+    this.r = r;
+    this.c = c;
+  }
+
+  /**
+   *
+   * @param {Coordinate} other 다른 좌표
+   * @returns 해당 객체의 행,열 좌표와 다른 좌표 객체의 행,열 좌표가 서로 동일한지 확인한 결과
+   */
+  same(other) {
+    return this.r === other.r && this.c === other.c;
+  }
+}
+class Tile extends Coordinate {
+  constructor(r, c, bead = 0) {
+    super.r = r;
+    super.c = c;
     this.bead = bead;
   }
 }
 
 function solution(input) {
-  let bead1ExplosionCount = 0;
-  let bead2ExplosionCount = 0;
-  let bead3ExplosionCount = 0;
+  const beadExploCnt = [0, 0, 0, 0];
 
-  const [N, M] = input[0].split(' ').map(Number);
+  const [N, M] = input[0].split(" ").map(Number);
 
-  const sharkPosition = [(N + 1) / 2 - 1, (N + 1) / 2 - 1]; // 상어의 위치
+  const sharkCoord = new Coordinate((N + 1) / 2 - 1, (N + 1) / 2 - 1); // 상어의 위치
   const field = new Array(N); // 구슬의 배치를 저장하는 2차원 배열
-  const beads = []; // 문제의 주어진 과정을 진행을 위해 구슬 정보를 담는 1차원 배열
-  const direction = { 'left': [-1, 0], 'right': [1, 0], 'up': [0, -1], 'down': [0, 1] };
+  const tileArr = []; // 문제의 주어진 과정을 진행을 위해 구슬 배치 정보를 담는 1차원 배열
 
-  const isValidPosition = (r, c) => (0 <= r && r < N) && (0 <= c && c < N);
+  const up = new Coordinate(0, -1); // 상
+  const down = new Coordinate(0, 1); // 하
+  const left = new Coordinate(-1, 0); // 좌
+  const right = new Coordinate(1, 0); // 우
+
+  const isValidCoord = (coord) => 0 <= coord.r && coord.r < N && 0 <= coord.c && coord.c < N;
 
   // 구슬 배치 저장
   for (let i = 0; i < N; i++) {
-    field[i] = input[i + 1].split(' ').map(Number);
+    field[i] = input[i + 1].split(" ").map(Number);
   }
 
-  let currentPosition = sharkPosition;
   // 상어 위치를 기준으로, 좌하우상 방향으로 순회 하며 구슬을 1차원 배열로 변환
-  while (isValidPosition(...currentPosition)) {
+  let curCoord = sharkCoord;
+  const serializeDirArr = [left, down, right, up]; // 구슬 직렬화 방향
+  let distance = 0;
+  let keepDistance = false;
+  let serializeDir = -1;
 
+  while (isValidCoord(curCoord)) {
+    if (!keepDistance) distance += 1;
+    keepDistance = !keepDistance;
+    serializeDir = (serializeDir + 1) % 4;
+
+    for (let i = 0; i < distance; i++) {
+      curCoord.change(
+        curCoord.r + serializeDirArr[serializeDir].r,
+        curCoord.c + serializeDirArr[serializeDir].c
+      );
+
+      tileArr.push(
+        new Tile(curCoord.r, curCoord.c, field[curCoord.r][curCoord.c])
+      );
+    }
   }
 
+  // 구슬 폭발, 이동, 재폭발, 변화
+  const exploDirArr = [up, down, left, right]; // 구슬 폭발 방향
 
-  return bead1ExplosionCount + 2 * bead2ExplosionCount + 3 * bead3ExplosionCount;
+  for (let i = 0; i < M; i++) {
+    const [si, di] = input[N + i].split(" ").map(Number);
+
+    // 구슬 폭발
+    let exploDir = exploDirArr[di];
+    let exploCoordArr = [];
+
+    for (let s = 1; s <= si; s++) {
+      exploCoordArr.push(
+        new Coordinate(
+          sharkCoord.r + s * exploDir.r,
+          sharkCoord.c + s * exploDir.c
+        )
+      );
+    }
+
+    for (const tile of tileArr) {
+      if (exploCoordArr.findIndex((v) => v.same()) !== -1) {
+        beadExploCnt[tile.bead] += 1;
+        tile.bead = 0;
+      }
+    }
+
+    // 재폭발
+    let startIdx = 0, endIdx = 1, cnt = 0;
+    let prebead = tileArr[startIdx].bead;
+
+    while (endIdx < tileArr.length) {
+      let curbead = tileArr[endIdx].bead;
+      if (curbead === prebead) cnt++;
+      else {
+        if (cnt >= 4) {
+          beadExploCnt[prebead] += cnt;
+          for (let idx = startIdx; idx < endIdx; idx++) {
+            tileArr[idx].bead = 0;
+          }
+
+          startIdx = 0, endIdx = 1, cnt = 0;
+          prebead = tileArr[startIdx].bead;
+        }
+        else {
+          startIdx = endIdx;
+        }
+      };
+
+      endIdx += 1;
+      prebead = curbead;
+    }
+
+    // 변화
+    let newBeads = [];
+    startIdx = 0, cnt = 0;
+    curCoord = sharkCoord;
+    while (true) {
+      if (newBeads.length > N) break;
+      // 구슬 그룹 파악
+      // 구슬 배치
+    }
+  }
+
+  return beadExploCnt.reduce((pre, cur, i, _) => pre + cur * i, 0);
 }
 
 console.log(solution(input));
