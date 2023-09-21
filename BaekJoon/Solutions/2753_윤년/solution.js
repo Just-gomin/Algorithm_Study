c20bec02165e2489aecb5251237f7fed9e6e6dd2
안강민 <hkkm612@gmail.com>
2023-09-21 16:20:42 +0900

    update. [BOJ /2753_윤년] JS 해결 및 스터디 문서 갱신

--- BaekJoon/Solutions/2753_윤년/solution.js
+++ BaekJoon/Solutions/2753_윤년/solution.js
@@ -1,1 +1,21 @@
-const solution = () => {};
+/*
+  - 문제 Link : https://www.acmicpc.net/problem/2753
+*/
+
+const fs = require("fs");
+const { exit } = require("process");
+const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
+const input = fs
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((v) => v.trim());
+
+function solution(input) {
+    const year = Number(input[0]);
+
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) ? 1 : 0;
+}
+
+console.log(solution(input));
