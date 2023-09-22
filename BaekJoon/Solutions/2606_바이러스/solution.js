20ec23415c443e2c5d94868a4cf06828e2a58192
안강민 <hkkm612@gmail.com>
2023-09-22 11:13:02 +0900

    create. [BOJ / 2606_바이러스] JS 해결 및 스터디 문서 갱신

--- BaekJoon/Solutions/2606_바이러스/solution.js
+++ BaekJoon/Solutions/2606_바이러스/solution.js
@@ -1,33 +1,24 @@
-// ------------------------------
-// Input with File System
-// ------------------------------
+/*
+  - 문제 Link : https://www.acmicpc.net/problem/2606
 
-// Read Inputs (from. https://help.acmicpc.net/language/info [node.js & TypeScript])
+  # Solution
+  - 컴퓨터의 수는 100 이하인 양의 정수, 컴퓨터에는 1번 부터 차례대로 번호가 매겨진다.
+  - 네트워크를 통해 웜 바이러스 전파.
+  - 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수 구하기
+
+  - 주어진 그래프 데이터를 인접 리스트 형태로 저장한다.
+  - 1번 부터 너비 우선 탐색을 진행해, 바이러스에 걸리게 되는 컴퓨터의 수를 센다.
+*/
+
 const fs = require("fs");
 const { exit } = require("process");
-
-// 입력값 파일 경로 결정
-// node 실행 시 환경변수 [RUNNING_ON]을 "local"라는 값을 부여하여 '백준 온라인' 과 '로컬' 환경 구분
 const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
-
-// 입력 값
 const input = fs
   .readFileSync(filePath)
   .toString()
   .trim()
   .split("\n")
   .map((v) => v.trim());
-
-// ------------------------------
-// Input with Read Line
-// ------------------------------
-
-// // Read Inputs (from. https://velog.io/@leenzy/readline-%EB%AA%A8%EB%93%88-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0)
-// const readline = require("readline");
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
 
 function solution(input) {
   let answer = 0;
