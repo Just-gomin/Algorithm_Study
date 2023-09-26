8da7d48ccb4bcf4b723c1a4b8425badf76467c35
안강민 <hkkm612@gmail.com>
2023-09-26 11:06:41 +0900

    create. [BOJ / 1325_효율적인_해킹] JS 시간초과 및 스터디 문서 갱신

--- BaekJoon/Playground/js_playground/index.js
+++ BaekJoon/Playground/js_playground/index.js
@@ -29,38 +29,42 @@ const input = fs
 //   output: process.stdout,
 // });
 
-function solution(input = []) {
+function solution(input) {
+  let answer = [];
 
-  const [n, pairs] = [parseInt(input[0]), input.slice(1)];
+  const [N, M] = input[0].split(' ').map(Number);
 
-  let tree = [...new Array(n + 1)].map(() => []); // 인접 리스트 트리
-  let visited = new Array(n + 1).fill(false); // BFS를 위한 탐색 여부 확인 배열
-  let queue = [1]; // BFS를 수행하는 Queue
+  const dfs = (start, graph) => {
+    const visited = new Array(N + 1).fill(false);
+    const stack = [start];
 
-  let answer = new Array(n + 1).fill(NaN); // 노드의 부모를 기록하기 위한 배열
-
-  // 인접 리스트 트리 기록
-  for (const pair of pairs) {
-    const [from, to] = pair.split(' ').map(Number);
-
-    tree[from].push(to);
-    tree[to].push(from);
-  }
-
-  // BFS 진행 및 부모노드 기록
-  while (queue.length > 0) {
-    let curNode = queue.shift();
-
-    for (const child of tree[curNode]) {
-      if (!visited[child]) {
-        visited[child] = true;
-        answer[child] = curNode;
-        queue.push(child);
+    while (stack.length !== 0) {
+      const node = stack.pop();
+      visited[node] = true;
+      for (const child of graph[node]) {
+        if (!visited[child]) stack.push(child);
       }
     }
+
+    return visited.filter((value) => value).length;
   }
 
-  return answer.slice(2).join('\n');
+  const graph = new Array(N + 1).fill(0).map(() => new Array());
+  for (let i = 1; i <= M; i++) {
+    const [A, B] = input[i].split(' ').map(Number);
+    graph[B].push(A);
+  }
+
+  let maxHacked = 0;
+  for (let i = 1; i <= N; i++) {
+    let hacked = dfs(i, graph);
+    if (maxHacked < hacked) {
+      maxHacked = hacked;
+      answer = [i];
+    } else if (maxHacked === hacked) answer.push(i);
+  }
+
+  return answer.join(' ');
 }
 
 console.log(solution(input));
---
+++ BaekJoon/Solutions/1325_효율적인_해킹/solution.js
@@ -0,0 +1,68 @@
+/*
+  - 문제 Link : https://www.acmicpc.net/problem/1325
+
+  # Solution
+  - 이 회사는 N개의 컴퓨터로 이루어져 있다
+  - 이 회사의 컴퓨터는 신뢰하는 관계와, 신뢰하지 않는 관계로 이루어져 있는데, A가 B를 신뢰하는 경우에는 B를 해킹하면, A도 해킹할 수 있다
+  - 이 회사의 컴퓨터의 신뢰하는 관계가 주어졌을 때, 한 번에 가장 많은 컴퓨터를 해킹할 수 있는 컴퓨터의 번호를 출력하는 프로그램을 작성
+  - 입력) 첫째 줄에, N과 M이 들어온다. N은 10,000보다 작거나 같은 자연수, M은 100,000보다 작거나 같은 자연수
+  - 입력) 둘째 줄부터 M개의 줄에 신뢰하는 관계가 A B와 같은 형식으로 들어오며, "A가 B를 신뢰한다"를 의미한다
+  - 입력) 컴퓨터는 1번부터 N번까지 번호가 하나씩 매겨져 있다
+  - 출력) 첫째 줄에, 김지민이 한 번에 가장 많은 컴퓨터를 해킹할 수 있는 컴퓨터의 번호를 오름차순으로 출력한다.
+
+  - 인접 리스트 그래프로 주어진 신뢰 관계를 저장한다.
+    - 양방향이 아니기 때문에 단방향으로의 관계만 저장한다.
+  - 1번부터 N번까지 순회하며, DFS를 진행한다.
+  - DFS를 진행하며, 각 노드의 최대 탐색 횟수를 저장한다. 
+    다음 탐색 진행시 해당 노드가 나오면 바로 탐색 가능한 노드 수를 반환한다.
+*/
+
+const fs = require("fs");
+const { exit } = require("process");
+const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
+
+function solution(input) {
+  let answer = [];
+
+  const [N, M] = input[0].split(' ').map(Number);
+
+  const dfs = (start, graph) => {
+    const visited = new Array(N + 1).fill(false);
+    const stack = [start];
+
+    while (stack.length !== 0) {
+      const node = stack.pop();
+      visited[node] = true;
+      for (const child of graph[node]) {
+        if (!visited[child]) stack.push(child);
+      }
+    }
+
+    return visited.filter((value) => value).length;
+  }
+
+  const graph = new Array(N + 1).fill(0).map(() => new Array());
+  for (let i = 1; i <= M; i++) {
+    const [A, B] = input[i].split(' ').map(Number);
+    graph[B].push(A);
+  }
+
+  let maxHacked = 0;
+  for (let i = 1; i <= N; i++) {
+    let hacked = dfs(i, graph);
+    if (maxHacked < hacked) {
+      maxHacked = hacked;
+      answer = [i];
+    } else if (maxHacked === hacked) answer.push(i);
+  }
+
+  return answer.join(' ');
+}
+
+console.log(solution(input));
