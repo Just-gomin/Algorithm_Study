3ea6f3867317780a8137cbbcccb7485db1327668
안강민 <hkkm612@gmail.com>
2023-09-28 09:46:50 +0900

    update. 문제파일형식 JS  불필요 require문 제거. readline으로 Input 읽어오는 코드 주석 수정.

--- 문제파일형식/solution.js
+++ 문제파일형식/solution.js
@@ -3,29 +3,37 @@
 */
 
 // ------------------------------
+// Input with Read Line
+// ------------------------------
+/*
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on('line', (line) => {
+    // input 처리
+  });
+
+  rl.on('close', () => {
+    // 출력 처리
+    process.exit();
+  });
+*/
+
+// ------------------------------
 // Input with File System
 // ------------------------------
 const fs = require("fs");
-const { exit } = require("process");
 const filePath = process.env.RUNNING_ON === "local" ? "./stdin" : "/dev/stdin";
 const input = fs
   .readFileSync(filePath)
   .toString()
   .trim()
-  .split("\n")
-  .map((v) => v.trim());
+  .split("\n");
 
-// ------------------------------
-// Input with Read Line
-// ------------------------------
-
-// const readline = require("readline");
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-function solution() {
+function solution(input) {
   let answer = "";
   return answer;
 }
