aa994963a234f3f12ce163761c05a623ee47ec22
안강민 <hkkm612@gmail.com>
2023-10-03 16:23:08 +0900

    create. [ConceptualStudy / ComputerScience / DataStructure] Array CRUD

---
+++ ConceptualStudy/ComputerScience/DataStructure/Array/index.js
@@ -0,0 +1,17 @@
+// Create Data
+let array = Array(...Array(10).keys());
+
+console.log("Create Data", array);
+
+// Read Data
+console.log("Read Data", array[1]); // 1
+
+// Update Data
+array[1] = 12;
+
+console.log("Update Data", array[1]); // 12
+
+// Delete Data
+delete array[1];
+
+console.log("Delete Data", array[1]); // undefined
