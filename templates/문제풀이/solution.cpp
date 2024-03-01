843980a083146ed8192d050e1ae52099aa94aca9
안강민 <hkkm612@gmail.com>
2024-03-01 10:19:39 +0900

    feat : templates/문제풀이 에 cpp & dart 추가
    - Standard input & output 처리

---
+++ templates/문제풀이/solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void main()
+{
+    string input;
+    cin >> input;
+    cout << input;
+}
---
+++ templates/문제풀이/solution.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+void main() {
+  var input = stdin.readLineSync();
+  stdout.write(input);
+}
