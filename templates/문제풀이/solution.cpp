a52a83c7d7b8f9e751ed8e619281cfb9b6b6fe9a
안강민 <hkkm612@gmail.com>
2024-03-05 13:10:59 +0900

    fix(template): C++ 빠른 입출력을 위한 설정
    - cin.tie(nullptr); & ios::sync_with_stdio(false); 추가
    - endl 대신 개행문자 사용

--- templates/문제풀이/solution.cpp
+++ templates/문제풀이/solution.cpp
@@ -6,9 +6,13 @@ using namespace std;
 
 int main()
 {
+    // 빠른 입출력을 위한 설정: https://www.acmicpc.net/board/view/47745
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
     string input;
     cin >> input;
-    cout << input;
+    cout << input << "\n";
 
     return 0;
 }
